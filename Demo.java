a2ced25dba47143fa92273ba78aa8a2f93dd8660
PingTang <brillianteyet@gmail.com>
2017-05-27 10:04:19 +0800

    change Demo.java

--- Demo.java
+++ Demo.java
@@ -2,12 +2,8 @@ public class Demo{
 	public static void main(String[] args) {
 		System.out.println(" Hello Git !!");
 		System.out.println(" Hello Git !!");
-		String name = "TangPing";
-		String name = "TangPing";
-		String name = "TangPing";
-		String name = "TangPing";
-		String name = "TangPing";
-		String name = "TangPing";
+		String name = "tangPing";
+		String x = "hello";
 		int age = 23;
 	}
 }
