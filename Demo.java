57b9b77cfe7a84c49cbebc076ea32d3f9376814b
PingTang <brillianteyet@gmail.com>
2017-05-27 09:26:44 +0800

    Add Feature.java and patch ; modified Demo.java

--- Demo.java
+++ Demo.java
@@ -2,5 +2,11 @@ public class Demo{
 	public static void main(String[] args) {
 		System.out.println(" Hello Git !!");
 		System.out.println(" Hello Git !!");
+		String name = "TangPing";
+		int age = 23;
+
+		String change = "hello hello hello ";
+		String change = "hello hello hello ";
+		String change = "hello hello hello ";
 	}
 }
